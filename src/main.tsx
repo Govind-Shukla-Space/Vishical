806c901d608aa4d8b74a5409c9431efae8afeeae
Govind-Shukla-Space <govindshukla8115@gmail.com>
2026-02-09 14:44:30 +0530

    build

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+import "./App.css";
+import "./index.css";
+// Automatically import all CSS in src/css
+import.meta.glob("./css/*.css", { eager: true });
+
+
 createRoot(document.getElementById('root')!).render(
+
   <StrictMode>
     <App />
   </StrictMode>,
