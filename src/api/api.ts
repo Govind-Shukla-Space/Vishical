9d98e97e6e217dc379d479b9c1dd15039f812ca6
Govind-Shukla-Space <govindshukla8115@gmail.com>
2026-02-09 15:17:46 +0530

    resolved backend api

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const api = axios.create({
   // baseURL: "http://localhost:8080", // change if needed
-  baseURL: process.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
